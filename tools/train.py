2dc664d34796a388d91c91d3fe252048a021658e
Laughing-q <1185102784@qq.com>
2022-04-21 17:55:42 +0800

    update train.py

--- tools/train.py
+++ tools/train.py
@@ -4,6 +4,7 @@ import os
 import torch
 import torch.distributed as dist
 from loguru import logger
+import argparse
 
 LOCAL_RANK = int(
     os.getenv("LOCAL_RANK", -1)
@@ -11,17 +12,16 @@ LOCAL_RANK = int(
 RANK = int(os.getenv("RANK", -1))
 WORLD_SIZE = int(os.getenv("WORLD_SIZE", 1))
 
+
 def init_distributed():
 
     # Initializes the distributed backend which will take care of synchronizing nodes/GPUs
-    dist_url = "env://" # default
+    dist_url = "env://"  # default
 
     # only works with torch.distributed.launch // torch.run
     dist.init_process_group(
-            backend="nccl",
-            init_method=dist_url,
-            world_size=WORLD_SIZE,
-            rank=RANK)
+        backend="nccl", init_method=dist_url, world_size=WORLD_SIZE, rank=RANK
+    )
 
     # this will make all .cuda() calls work properly
     torch.cuda.set_device(LOCAL_RANK)
@@ -30,14 +30,29 @@ def init_distributed():
     dist.barrier()
 
 
-cfg = OmegaConf.load('configs/partial_glint360k.yaml')
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-c",
+        "--config",
+        type=str,
+        default="./configs/partial_glint360k.yaml",
+        help="config file",
+    )
+    opt = parser.parse_args()
+    return opt
 
-if LOCAL_RANK != -1:
-    init_distributed()
 
-trainer = Trainer(cfg, LOCAL_RANK)
-trainer.train()
+if __name__ == "__main__":
+    opt = parse_opt()
+    cfg = OmegaConf.load(opt.config)
 
-if WORLD_SIZE > 1 and RANK == 0:
-    logger.info("Destroying process group... ")
-    dist.destroy_process_group()
+    if LOCAL_RANK != -1:
+        init_distributed()
+
+    trainer = Trainer(cfg, LOCAL_RANK)
+    trainer.train()
+
+    if WORLD_SIZE > 1 and RANK == 0:
+        logger.info("Destroying process group... ")
+        dist.destroy_process_group()
